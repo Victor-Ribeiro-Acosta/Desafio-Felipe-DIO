cc642650fbd5f459e7e0aa82c2f1c9325d7d6bed
Victor-Ribeiro-Acosta <143853364+Victor-Ribeiro-Acosta@users.noreply.github.com>
2023-09-07 22:03:39 -0300

    Versão 2

--- Claculadora_de_Ranqueada/Calculadora_ranq.js
+++ Claculadora_de_Ranqueada/Calculadora_ranq.js
@@ -1,8 +1,10 @@
-function SaldoJogador(vitorias, derrotas){
+function SaldoHeroi(vitorias, derrotas){
+  // Função para obter o saldo do herói a partir do número de vitórias e derrtos
   return vitorias - derrotas
 }
 
-function ClassificacaoJogador(saldo){
+function ClassificacaoHeroi(saldo){
+  // Classificar heroi de acordo com seu saldo
   if(saldo <= 10){
     return "Ferro"
   } else if(saldo <= 20){
@@ -18,6 +20,6 @@ function ClassificacaoJogador(saldo){
   }else
   return "Imortal"
 }
-SaldoRanqueada = SaldoJogador(40,5)
+SaldoRanqueada = SaldoHeroi(40,5)
 
-console.log(`O saldo do herói é ${SaldoRanqueada} e ele está no nível ${ClassificacaoJogador(SaldoRanqueada)}`)
+console.log(`O saldo do herói é ${SaldoRanqueada} e ele está no nível ${ClassificacaoHeroi(SaldoRanqueada)}`)
